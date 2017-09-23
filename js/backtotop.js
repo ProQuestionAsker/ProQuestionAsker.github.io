7fba2d2202f12490435e5648e00661dc4949ddeb
Amber <proquestionasker@gmail.com>
2017-09-23 16:53:37 -0700

    New website structure

---
+++ js/backtotop.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+
+
+var offset = 250;
+ 
+var duration = 300;
+ 
+jQuery(window).scroll(function() {
+ 
+if (jQuery(this).scrollTop() > offset) {
+ 
+jQuery('.back-to-top').fadeIn(duration);
+ 
+} else {
+ 
+jQuery('.back-to-top').fadeOut(duration);
+ 
+}
+ 
+});
+ 
+ 
+ 
+jQuery('.back-to-top').click(function(event) {
+ 
+event.preventDefault();
+ 
+jQuery('html, body').animate({scrollTop: 0}, duration);
+ 
+return false;
+ 
+})
+ 
+});
+ 
+
+
+
